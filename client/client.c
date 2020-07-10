d9052d5ed622c7333c3530c2f394132972047dfe
Obessiveness <1365198103@qq.com>
2020-07-10 21:00:07 +0800

    improve client.c

--- client/client.c
+++ client/client.c
@@ -1,23 +1,23 @@
 /*************************************************************************
 	> File Name: client.c
-	> Author: 
-	> Mail: 
-	> Created Time: Fri 10 Jul 2020 08:49:59 AM CST
+	> Author: suyelu 
+	> Mail: suyelu@126.com
+	> Created Time: Wed 08 Jul 2020 04:32:12 PM CST
  ************************************************************************/
 
 #include "head.h"
 
 int server_port = 0;
 char server_ip[20] = {0};
+char name[20] = {0};
 char *conf = "./football.conf";
 int sockfd = -1;
 
-void logout(int sigum) {
+void logout(int signum) {
     struct ChatMsg msg;
     msg.type = CHAT_FIN;
-    send(sockfd, (void *)&msg, sizeof(msg), 0);
+    send(sockfd, (void*)&msg, sizeof(msg), 0);
     close(sockfd);
-    DBG(RED"Bye!\n"NONE);
     exit(0);
 }
 
@@ -46,10 +46,10 @@ int main(int argc, char **argv) {
                 break;
             default:
                 fprintf(stderr, "Usage : %s [-hptmn]!\n", argv[0]);
-                exit(1);                
+                exit(1);
         }
     }
-
+    
 
     if (!server_port) server_port = atoi(get_conf_value(conf, "SERVERPORT"));
     if (!request.team) request.team = atoi(get_conf_value(conf, "TEAM"));
@@ -58,57 +58,114 @@ int main(int argc, char **argv) {
     if (!strlen(request.msg)) strcpy(request.msg, get_conf_value(conf, "LOGMSG"));
 
 
-    DBG("<"GREEN"Conf Show"NONE"> : server_ip = %s, port = %d, team = %s, name = %s\n%s", server_ip, server_port, request.team ? "BLUE": "RED", request.name, request.msg);
+    DBG("<"GREEN"Conf Show"NONE"> : server_ip = %s, port = %d, team = %s, name = %s\n%s",\
+        server_ip, server_port, request.team ? "BLUE": "RED", request.name, request.msg);
 
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port = htons(server_port);
     server.sin_addr.s_addr = inet_addr(server_ip);
+
     socklen_t len = sizeof(server);
+
     if ((sockfd = socket_udp()) < 0) {
         perror("socket_udp()");
         exit(1);
-            
     }
-
-    sendto(sockfd, &request, sizeof(request), 0, (struct sockaddr *)&server, len);
-    fd_set rfds;
-    FD_ZERO(&rfds);
-    FD_SET(sockfd, &rfds);
-    int rc, ret;
+    
+    sendto(sockfd,&request, sizeof(request), 0, (struct sockaddr *)&server, len);
+    //
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(sockfd, &set);
     struct timeval tv;
     tv.tv_sec = 5;
     tv.tv_usec = 0;
-    rc = select(sockfd + 1, &rfds, NULL, NULL, &tv);
-    if(rc == 0){
-        return -1;                                
-    } else {
-        ret = recvfrom(sockfd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&server, &len);
-        if(ret < 0||response.type == 1){
-            perror("server refused!\n");                                    
+
+    int retval = select(sockfd + 1, &set, NULL, NULL, &tv);
+    if (retval < 0) {
+        perror("select()");
+        exit(1);
+    } else if (retval) {
+        int ret = recvfrom(sockfd, (void*)&response, sizeof(response), 0, (struct sockaddr*)&server, &len);
+        if (ret != sizeof(response) || response.type == 1) {
+            DBG(RED"Error"NONE"The Game Server refused your login.\n\tThis May be helpful: %s\n", response.msg);
+            exit(1);
         }
-        printf("%s\n", response.msg);                                
+    } else {
+        DBG(RED"Error"NONE"Server is out of service!\n");
+        exit(1);
     }
-    if(connect(sockfd, (struct sockaddr*)&server, sizeof(server)) == -1){
-        perror("connecting failed!\n");
-        exit(1);                                    
-    }
+
+    DBG(GREEN"Server"NONE" : %s\n", response.msg);
+
+    connect(sockfd, (struct sockaddr*)&server, len);
+
     //char buff[512] = {0};
-    //sprintf(buff, "hello , good night!");
+    //sprintf(buff, "wryyyyyyy.");
     //send(sockfd, buff, strlen(buff), 0);
-    //bzero(buff, sizeof(buff));
-    //recv(sockfd, buff, sizeof(buff), 0)
-    //DBG(RED"Server Info"NONE" : %s\n", buff);
-    signal(SIGINT,logout);
-    while(1) {
+    /*while (recv(sockfd, buff, sizeof(buff), 0) > 0) {
+        DBG(RED"Server Info"NONE" : %s", buff);
+    }*/
+    signal(SIGINT, logout);
+    while (1) {
         struct ChatMsg msg;
-        bzero(&msg, sizeof(msg));
         msg.type = CHAT_WALL;
-        printf(RED"Please Input:\n"NONE);
-        scanf("%[^\n]s",msg.msg);
+        printf(RED"Please Input: \n"NONE);
+        scanf("%[^\n]s", msg.msg);
         getchar();
-        send(sockfd, (void *)&msg, sizeof(msg), 0);
+        send(sockfd, (void*)&msg, sizeof(msg), 0);
     }
+
+
+/*
+    //HOMEWORK FOLLOWING
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);
+    int retval;
+
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+    if ((retval = select(sockfd + 1, &rfds, NULL, NULL, &tv)) <=  0) {
+        perror("select()");
+        exit(1);
+    }
+
+    while(1) {
+        char buff[MAX_MSG] = {0};
+        int ret = recvfrom(sockfd, response, sizeof(response), 0, (struct sockaddr*)&server, &len);
+        //判断response是否合法，以及type是否为1
+
+        if (ret < 0) {
+           // close(sockfd);
+            DBG(RED"recv failed\n"NONE);
+            exit(1);
+        } else if (response.type == 1) {
+           // close(sockfd);
+            DBG(RED"offline\n"NONE);
+            exit(1);
+        } 
+        
+    }
+ 
+    DBG(GREEN"After recvfrom\n"NONE);
+
+    if (connect(sockfd, (struct sockaddr*)&server, len) < 0) {
+        perror("connect()");
+        exit(1);
+    }
+    
+    
+    char buff[] = {"Hello"};
+    sendto(sockfd, (void*)buff, strlen(buff), 0, (struct sockaddr*)&server,len);
+    DBG(GREEN"After send to\n");
+    //接受成绩
+    while (recvfrom(sockfd, response, sizeof(response), 0, (struct sockaddr*)&server, &len) < 0) {
+        DBG(RED"Server Info"NONE" : %s\n", response.msg);
+    }
+*/
     return 0;
 }
-
